aceaaf681ab558765970592868334e1a780ac6a2
blune <blune@DESKTOP-SU5HNE8>
2021-06-07 18:31:23 -0300

    New Version Blunetta

---
+++ src/main/java/com/bruno/BaseTestcase.java
@@ -0,0 +1,5 @@
+package com.bruno;
+
+public class BaseTestcase {
+
+}
---
+++ src/main/java/com/bruno/TestingAPI.java
@@ -0,0 +1,10 @@
+package com.bruno;
+
+public class TestingAPI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
